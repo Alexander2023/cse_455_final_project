1c0338417c80394142a53dcd310d201023651cb7
Alexander <ana2023@uw.edu>
2023-06-03 17:05:53 -0700

    add recognizer with random labels

--- expression_recognizer.py
+++ expression_recognizer.py
@@ -1,18 +1,46 @@
+from cv2 import VideoCapture, imshow, waitKey, putText, FONT_HERSHEY_SIMPLEX, cvtColor, COLOR_BGR2RGB
+from PIL import Image
+from random import randint
+
 HAPPY_LABEL = "happy"
-INFERENCE_FRAME_RATE = 3
+INFERENCE_RATE = 10
+
+labels = ["angry", "disgust", "fear", "happy", "sad", "surprise", "neutral"]
 
 def load_model():
     pass
 
-def generate_expression_label():
-    pass
+def generate_expression_label(video_frame):
+    index = randint(0, 6)
+    return labels[index]
 
-def place_expression_label():
-    pass
+def save_happy_image(video_frame):
+    video_frame = cvtColor(video_frame, COLOR_BGR2RGB)
+    image = Image.fromarray(video_frame)
+    image.save("happy_image.jpg")
 
-def save_happy_image():
-    pass
-
+camera_feed = VideoCapture(0)
 load_model()
-while True:
-  continue
+
+i = 0
+current_label = ""
+try:
+    while True:
+        has_received_frame, video_frame = camera_feed.read()
+
+        if has_received_frame:
+            if i % INFERENCE_RATE == 0:
+                current_label = generate_expression_label(video_frame)
+                i = 0
+
+            putText(video_frame, current_label, (50, 50), FONT_HERSHEY_SIMPLEX,
+                    2, (0, 0, 0))
+            imshow('video_frame', video_frame)
+            waitKey(25)
+
+            if current_label == HAPPY_LABEL:
+                save_happy_image(video_frame)
+
+        i += 1
+finally:
+    camera_feed.release()
